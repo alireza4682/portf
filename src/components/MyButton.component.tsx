d96d96e6d0d2ea6e672d15d50a3adc33b5763060
Alireza <comolestyrusistantaneos@gmail.com>
2023-05-01 18:06:33 +0330

    start

--- src/components/MyButton.component.tsx
+++ src/components/MyButton.component.tsx
@@ -1,24 +1,13 @@
-import "./MyButton.style.css";
+import clsx from "clsx";
+import "./MyButton.style.scss";
+import star from "./starburst-four-point-icon.svg";
 
 const Button = () => {
   return (
-    <button>
+    <button className="mybutton">
       Button
       <div className="star-1">
-        <svg
-          viewBox="0 0 784.11 815.53"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs></defs>
-          <g id="Layer_x0020_1">
-            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-            <path
-              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-              className="fil0"
-            ></path>
-          </g>
-        </svg>
+        <img src={star}></img>
       </div>
       <div className="star-2">
         <svg
